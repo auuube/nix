3ab55ee7f6d6f7b3f236a43098ab0f9f2c3d55c1
auuube <kommanatch@gmail.com>
2025-04-03 20:57:51 +0900

    back to stable

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "my nixos config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
